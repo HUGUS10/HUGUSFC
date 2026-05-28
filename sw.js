c4e5de0e593df844801ca62a845ba3ca8569ed87
Erlis Laban <tu_correo_de_github@example.com>
2026-05-28 17:11:48 -0500

    actualizacion

--- sw.js
+++ sw.js
@@ -1,40 +1,31 @@
-const CACHE_NAME = "hugusfc-v2";
+const CACHE_NAME = "hugusfc-v3";
 
-const urlsToCache = [
+const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/manifest.json",
-  "/imag/logo.png"
+  "/imag/logo.png",
+  "/imag/escudo_hugusfc.png"
 ];
 
 self.addEventListener("install", event => {
   self.skipWaiting();
-
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
   );
-
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
